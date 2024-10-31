ea67e921dd00ab3df2439b0900e32be6be208f77
Red-Fox <ester_c_rocha@estudante.sesisenai.org.br>
2024-10-31 08:51:33 -0300

    Ok

--- back/db/conn.js
+++ back/db/conn.js
@@ -1,5 +1,5 @@
 const {Sequelize} = require('sequelize')
-const sequelize = new Sequelize('atv-10-17', 'root', 'senai', {
+const sequelize = new Sequelize('teste', 'root', 'senai', {
     host: "localhost",
     dialect:'mysql'
 })
